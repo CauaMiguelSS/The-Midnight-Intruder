b8f1e85e13024c298eafd9f5b0cba60db200e26e
Maria Eduarda Raulino Heinemann <maduzzzx@gmail.com>
2025-10-24 11:41:26 -0300

    musica jogo

---
+++ TheMidnightIntruder/Assets/Maria/Script/GameMusic.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMusic : MonoBehaviour
+{
+    void Awake()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "Maria(Start)")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
