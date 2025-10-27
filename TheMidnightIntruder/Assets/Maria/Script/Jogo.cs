749607358319891e996f00ab00079430a0ba677b
Maria Eduarda Raulino Heinemann <maduzzzx@gmail.com>
2025-10-27 08:55:16 -0300

    musica do jogo

---
+++ TheMidnightIntruder/Assets/Maria/Script/Jogo.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jogo : MonoBehaviour
+{
+    public void Jogar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void CreditosVoltar()
+    {
+        SceneManager.LoadScene("Titulo");
+
+    }
+}
